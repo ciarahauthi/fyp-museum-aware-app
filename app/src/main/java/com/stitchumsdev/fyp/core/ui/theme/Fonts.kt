a62e69c800e85ff529c426e21bfaf49abca5ccef
Ciara <ciaraduffy24@gmail.com>
2026-01-04 02:05:19 +0000

    Added :: Fonts

---
+++ app/src/main/java/com/stitchumsdev/fyp/core/ui/theme/Fonts.kt
@@ -0,0 +1,11 @@
+package com.stitchumsdev.fyp.core.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.stitchumsdev.fyp.R
+
+val Dongle = FontFamily(
+    Font(R.font.dongle_bold),
+    Font(R.font.dongle_regular),
+    Font(R.font.dongle_light)
+)
