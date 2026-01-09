60b3e437af4cc792746fd79d7ac97dd5b37f5fdf
Ciara <ciaraduffy24@gmail.com>
2026-01-09 01:52:34 +0000

    Added beacons, catrgory, exhibit tables

--- api/init_tables.py
+++ api/init_tables.py
@@ -8,10 +8,39 @@ def init():
     print("Tables created")
 
     with SessionLocal() as db:
-        db.add(User(first_name="Ciara", surname="Duffy", email="ciara@gmail.com"))
-        db.add(User(first_name="Test", surname="Test", email="test@gmail.com"))
+        user = User(first_name="Ciara", surname="Duffy", email="ciara@gmail.com")
+        db.add(user)
         db.commit()
 
+        beacon = Beacon(
+            name="Test beacon",
+            description="Test beacon",
+            uuid="B9407F30-F5F8-466E-AFF9-25556B57FE6D",
+            major=1,
+            minor=4,
+            employee_id=user.id)
+        db.add(beacon)
+        db.commit()
+
+        category = Category(
+            name="Animal", 
+            description="Animal category",
+            employee_id=user.id)
+        db.add(category)
+        db.commit()
+
+        exhibit = Exhibit(
+            title="Rusty",
+            description="Cute dog.",
+            beacon_id=beacon.id,
+            category_id=category.id,
+            employee_id=user.id,
+            child_friendly=True,
+            likes=0,
+            dislikes=0)
+
+        db.add(exhibit)
+        db.commit()
     print("Added dummy data")
 
 if __name__ == "__main__":
--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text, ForeignKey
 
 class User(Base):
     __tablename__ = "users"
@@ -8,3 +8,42 @@ class User(Base):
     first_name = Column(String(100), nullable=False)
     surname = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
+
+class Exhibit(Base):
+    __tablename__ = "exhibit"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String(2000), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+    beacon_id = Column(Integer, ForeignKey("beacon.id", ondelete="SET NULL"), nullable=True)
+    category_id = Column(Integer, ForeignKey("category.id", ondelete="SET NULL"), nullable=True)
+    employee_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    child_friendly = Column(Boolean, nullable=False)
+    likes = Column(Integer, nullable=False, server_default="0")
+    dislikes = Column(Integer, nullable=False, server_default="0")
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(2000), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+    employee_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+class Beacon(Base):
+    __tablename__ = "beacon"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(2000), nullable=False)
+    uuid = Column(String(100), nullable=False)
+    major = Column(Integer, nullable=False, server_default="0")
+    minor = Column(Integer, nullable=False, server_default="0")
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+    employee_id = Column(Integer, ForeignKey("users.id"), nullable=False)
