ded33b6019da51df2c87c8cad2abd361344cdd8b
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-04 23:18:23 +0000

    Added :: Dummy data for new tables.

--- api/init_tables.py
+++ api/init_tables.py
@@ -8,17 +8,71 @@ def init():
     print("Tables created")
 
     with SessionLocal() as db:
+        # Dummy Graph
+        nodes = ["A", "B", "C", "D", "E", "F", "G"]
+        edges = [("A", "B", 3),
+                ("A", "C", 6),
+
+                ("B", "C", 2),
+                ("B", "D", 4),
+
+                ("C", "F", 7),
+
+                ("D", "E", 1),
+                ("D", "F", 2),
+
+                ("E", "G", 6),
+
+                ("F", "G", 5),
+                ]
+        
+        coords = {
+            "A": (0.12, 0.37),
+            "B": (0.20, 0.28),
+            "C": (0.20, 0.46),
+            "D": (0.40, 0.38),
+            "E": (0.62, 0.27),
+            "F": (0.56, 0.60),
+            "G": (0.70, 0.37),
+        }
+
+        nodeObj = {}
+
+        for name in nodes:
+            node = Node(
+                name=name,
+                x = coords[name][0],
+                y = coords[name][1]
+            )
+            db.add(node)
+            db.flush()
+            nodeObj[node.name] = node.id
+
+        db.commit()
+        for node, connNode, weight in edges:
+            db.add(
+                Edge(
+                    node_id = nodeObj[node],
+                    connected_node_id = nodeObj[connNode],
+                    weight = weight,
+                )
+            )
+        db.commit()
+
         user = User(first_name="Ciara", surname="Duffy", email="ciara@gmail.com")
         db.add(user)
         db.commit()
-
+    
+        # Dummy Beacons
         beacon = Beacon(
             name="Test beacon",
             description="Test beacon - the green one",
             uuid="B9407F30-F5F8-466E-AFF9-25556B57FE6D",
             major=1,
             minor=4,
-            employee_id=user.id)
+            employee_id=user.id,
+            location_id = nodeObj["A"]
+            )
         db.add(beacon)
         db.commit()
 
@@ -28,7 +82,9 @@ def init():
             uuid="B9407F30-F5F8-466E-AFF9-25556B57FE6D",
             major=1,
             minor=3,
-            employee_id=user.id)
+            employee_id=user.id,
+            location_id = nodeObj["B"]
+            )
         db.add(beacon2)
         db.commit()
 
@@ -38,7 +94,9 @@ def init():
             uuid="B9407F30-F5F8-466E-AFF9-25556B57FE6D",
             major=1,
             minor=2,
-            employee_id=user.id)
+            employee_id=user.id,
+            location_id = nodeObj["C"]
+            )
         db.add(beacon3)
         db.commit()
 
@@ -48,10 +106,13 @@ def init():
             uuid="B9407F30-F5F8-466E-AFF9-25556B57FE6D",
             major=1,
             minor=1,
-            employee_id=user.id)
+            employee_id=user.id,
+            location_id = nodeObj["D"]
+            )
         db.add(beacon4)
         db.commit()
 
+        # Dummy Categories
         category = Category(
             name="Animal", 
             description="Animal category",
@@ -59,6 +120,7 @@ def init():
         db.add(category)
         db.commit()
 
+        # Dummy Exhibits
         exhibit = Exhibit(
             title="Rusty",
             description="Cute dog.",
