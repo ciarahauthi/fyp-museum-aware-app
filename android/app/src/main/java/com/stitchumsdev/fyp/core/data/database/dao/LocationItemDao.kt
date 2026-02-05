9cd1f58cc2a3bf131332bb421331f1a6e36d3b2b
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-05 15:55:18 +0000

    Added :: Location database & relevant entities

---
+++ android/app/src/main/java/com/stitchumsdev/fyp/core/data/database/dao/LocationItemDao.kt
@@ -0,0 +1,19 @@
+package com.stitchumsdev.fyp.core.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.stitchumsdev.fyp.core.data.database.entities.LocationItemEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LocationItemDao {
+    @Query("SELECT * FROM locationitementity")
+    fun getAll(): Flow<List<LocationItemEntity>>
+
+    @Query("SELECT * FROM locationitementity WHERE id = :id")
+    suspend fun getLocById(id: Int): LocationItemEntity
+
+    @Upsert
+    suspend fun upsertAll(items: List<LocationItemEntity>)
+}
