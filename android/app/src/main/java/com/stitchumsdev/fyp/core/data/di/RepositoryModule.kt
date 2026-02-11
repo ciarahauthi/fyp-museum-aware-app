1801d7ba4d0e89a00456c49e31436fa80877c118
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-11 15:10:56 +0000

    Added :: Museum repo to koin di

--- android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/RepositoryModule.kt
+++ android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/RepositoryModule.kt
@@ -1,9 +1,12 @@
 package com.stitchumsdev.fyp.core.data.di
 
+import com.stitchumsdev.fyp.core.data.repository.MuseumRepository
+import com.stitchumsdev.fyp.core.data.repository.MuseumRepositoryImpl
 import com.stitchumsdev.fyp.core.data.repository.UserRepository
 import com.stitchumsdev.fyp.core.data.repository.UserRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<UserRepository> { UserRepositoryImpl(api = get()) }
+    single<MuseumRepository> { MuseumRepositoryImpl(appDatabase = get()) }
 }
