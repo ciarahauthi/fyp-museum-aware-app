4d2922ab899dc7e2d5e2c2c02ff6243fc96f1941
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-11 15:11:09 +0000

    Added :: Route vm to koin di

--- android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/ViewModelModule.kt
+++ android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.stitchumsdev.fyp.core.data.di
 
 import com.stitchumsdev.fyp.feature.home.HomeViewModel
 import com.stitchumsdev.fyp.feature.map.MapViewModel
+import com.stitchumsdev.fyp.feature.route.RouteViewModel
 import com.stitchumsdev.fyp.feature.scan.ScanViewModel
 import com.stitchumsdev.fyp.feature.test.TestScreenViewModel
 import org.koin.core.module.dsl.viewModel
@@ -12,4 +13,5 @@ val viewModelModule = module {
     viewModel { ScanViewModel(get(), get()) }
     viewModel { HomeViewModel(get(), get()) }
     viewModel { MapViewModel(get()) }
+    viewModel { RouteViewModel(get()) }
 }
