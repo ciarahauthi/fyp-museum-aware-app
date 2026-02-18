ad501f39b3982083abc30e195023d37d9d5e3ec0
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-18 18:14:11 +0000

    Updated :: Beacon repository to derive current location off of

--- android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/BeaconModule.kt
+++ android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/BeaconModule.kt
@@ -5,5 +5,5 @@ import com.stitchumsdev.fyp.core.data.repository.BeaconRepositoryImpl
 import org.koin.dsl.module
 
 val beaconModule = module {
-    single<BeaconRepository> { BeaconRepositoryImpl() }
+    single<BeaconRepository> { BeaconRepositoryImpl(get()) }
 }
