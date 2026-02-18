19deb248c7f05c61f46c162875a8c9a11e1f57e3
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-18 18:10:43 +0000

    Added :: Colours

--- android/app/src/main/java/com/stitchumsdev/fyp/core/ui/theme/Colors.kt
+++ android/app/src/main/java/com/stitchumsdev/fyp/core/ui/theme/Colors.kt
@@ -11,6 +11,7 @@ import com.stitchumsdev.fyp.R
 @Immutable
 data class FypColours(
     val mainText: Color = Color.Unspecified,
+    val secondaryText: Color = Color.Unspecified,
     val mainBackground: Color = Color.Unspecified,
     val secondaryBackground: Color = Color.Unspecified,
     val navBar: Color = Color.Unspecified,
@@ -27,7 +28,8 @@ fun getLightColors() = FypColours (
 @Composable
 fun getDarkColors() = FypColours (
     mainText = colorResource(R.color.parchment),
-    mainBackground = colorResource(R.color.black),
+    secondaryText = colorResource(R.color.text_grey),
+    mainBackground = colorResource(R.color.dark),
     secondaryBackground = colorResource(R.color.deep_grey),
     navBar = colorResource(R.color.charcoal)
 )
