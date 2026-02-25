327b55294e1f5989f1f3696f8d886ec014c4452a
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-25 14:20:17 +0000

    Added :: Search VM removed test VM

--- android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/ViewModelModule.kt
+++ android/app/src/main/java/com/stitchumsdev/fyp/core/data/di/ViewModelModule.kt
@@ -4,17 +4,17 @@ import com.stitchumsdev.fyp.feature.home.HomeViewModel
 import com.stitchumsdev.fyp.feature.map.MapViewModel
 import com.stitchumsdev.fyp.feature.route.RouteViewModel
 import com.stitchumsdev.fyp.feature.scan.ScanViewModel
+import com.stitchumsdev.fyp.feature.search.SearchViewModel
 import com.stitchumsdev.fyp.feature.splash.SplashViewModel
 import com.stitchumsdev.fyp.feature.test.TestScreenViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { TestScreenViewModel(get()) }
     viewModel { ScanViewModel(get(), get()) }
     viewModel { HomeViewModel(get(), get()) }
     viewModel { MapViewModel(get(), get()) }
     viewModel { RouteViewModel(get(), get(), get()) }
-    // viewModel { SearchViewModel(get()) }
+    viewModel { SearchViewModel(get()) }
     viewModel { SplashViewModel(get(), get(), get()) }
 }
