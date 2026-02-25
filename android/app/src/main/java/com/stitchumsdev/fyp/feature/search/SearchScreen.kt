bf60149ec517bb89930559b20f4904367f490b55
ciarahauthi <ciaraduffy24@gmail.com>
2026-02-25 14:21:14 +0000

    Added :: Search Screen

--- android/app/src/main/java/com/stitchumsdev/fyp/feature/search/SearchScreen.kt
+++ android/app/src/main/java/com/stitchumsdev/fyp/feature/search/SearchScreen.kt
@@ -1,24 +1,30 @@
-package com.stitchumsdev.fyp.feature.search
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.stitchumsdev.fyp.core.model.ObjectModel
 import com.stitchumsdev.fyp.core.ui.components.BottomNavigationBar
+import com.stitchumsdev.fyp.feature.search.SearchAction
+import com.stitchumsdev.fyp.feature.search.SearchUiState
 
 @Composable
 fun SearchScreen(
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    uiState: SearchUiState,
+    onAction: (SearchAction) -> Unit,
+    onObjectClick: (ObjectModel) -> Unit = {}
 ) {
     Scaffold(
         bottomBar = { BottomNavigationBar(navHostController) },
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        Text(
-            text = "Hello SearchScreen!",
+        SearchContent(
+            uiState = uiState,
+            onAction = onAction,
+            selectionMode = false,
+            onObjectClick = onObjectClick,
             modifier = Modifier.padding(innerPadding)
         )
     }
