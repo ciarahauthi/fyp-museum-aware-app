9ed6d94112ba81eaf0fdb57f78f10dbeb6d977c7
Ciara <ciaraduffy24@gmail.com>
2026-03-01 23:11:03 +0000

    Added :: Home screen UI state and home screen VM

---
+++ android/app/src/main/java/com/stitchumsdev/fyp/feature/home/HomeUiState.kt
@@ -0,0 +1,17 @@
+package com.stitchumsdev.fyp.feature.home
+
+import com.stitchumsdev.fyp.core.model.HomeItem
+import com.stitchumsdev.fyp.core.model.ExhibitModel
+
+sealed interface HomeUiState {
+    data object Loading : HomeUiState
+    data object Error: HomeUiState
+    data class Success(
+        val topSection: List<HomeItem> = emptyList(),
+        val midSection: HomeItem? = null,
+        val bottomSection: List<HomeItem> = emptyList(),
+        val selectedCard: HomeItem? = null,
+        val popular: List<ExhibitModel> = emptyList(),
+        val new: List<ExhibitModel> = emptyList(),
+    ) : HomeUiState
+}
