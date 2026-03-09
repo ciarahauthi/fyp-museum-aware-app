402f41c458dcb109723a1831de093a942b803e35
Ciara <ciaraduffy24@gmail.com>
2026-03-09 11:41:14 +0000

    Added :: Login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import TestDb from "./pages/testdb";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/getUsers" element={<TestDb />} />
+        <Route path="/getUsers" element={<TestDb />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
