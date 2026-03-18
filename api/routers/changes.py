6d8991a47faed29348809ea886b51ec788594df6
ciarahauthi <ciaraduffy24@gmail.com>
2026-03-18 22:10:38 +0000

    Added :: Changes table, schema, router and helper

---
+++ api/routers/changes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from api.db.database import get_db
+from api.db.models import Changes
+from api.schemas.changes import ChangeRead, ChangeCreate
+from api.db.models import User
+from api.core.auth import get_current_user
+
+router = APIRouter()
+
+@router.get("", response_model=list[ChangeRead])
+def get_routes(db: Session = Depends(get_db)):
+    changes = db.query(Changes).order_by(Changes.timestamp.desc()).all()
+
+    return changes
